3fcf1e3a5f1f445b222985ab8a24e4cd006dcae8
PeterRetter <88488655+PeterRetter@users.noreply.github.com>
2023-03-28 07:39:26 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+pip install python-chess
+
+import chess
+import random
+
+board = chess.Board()
+
+while not board.is_game_over():
+    move = random.choice(list(board.legal_moves))
+    board.push(move)
+    print(board)
